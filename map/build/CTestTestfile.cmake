847ebe3825c30f1bd9e781290f430c209d82e558
HTY679 <1064844148@qq.com>
2026-08-23 10:56:32 +0800

    main中删除map

--- map/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/forrich
-# Build directory: D:/forrich/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(map_tests "D:/forrich/build/map_tests.exe")
-set_tests_properties(map_tests PROPERTIES  _BACKTRACE_TRIPLES "D:/forrich/CMakeLists.txt;22;add_test;D:/forrich/CMakeLists.txt;0;")
